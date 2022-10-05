5ed4ed13d9ad371fa33c1533357018c5f5f2570c
Jung Woo <michaelwoo921@gmail.com>
2022-10-04 18:35:14 -0700

    deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const dotenv = require('dotenv')
 const connectDB = require('./db');
 dotenv.config();
@@ -11,9 +12,6 @@ app.use(express.json({
     extended: false
 }))
 
-app.get('/api/test', (req,res) => {
-    res.json('api running')
-})
 
 // define routes
 app.use('/api/users', require('./routes/api/users'))
@@ -21,7 +19,18 @@ app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 
-const PORT = process.env.PORT || 3001
+// serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+
+const PORT = process.env.PORT 
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT} ...`)
